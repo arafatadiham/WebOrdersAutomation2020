50cd92658eafeddc997f1632b2a3039a0cac91d6
arafatadiham <arafat.adiham@gmail.com>
2020-04-05 10:53:40 -0700

    first commit

---
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -0,0 +1,24 @@
+package com.weborders.pages;
+
+
+
+    /*
+
+    It mean to be extended
+     */
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractBasePage{
+
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        public AbstractBasePage(){
+            PageFactory.initElements(driver, this);
+        }
+}
